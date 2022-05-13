476c0dbfaa4a7c9da2427c7ee469d58bf097c304
hrafag <afagnd@code.edu.az>
2022-05-14 02:12:58 +0400

    the second update is added

--- Course management application/Program.cs
+++ Course management application/Program.cs
@@ -6,7 +6,23 @@ namespace Course_management_application
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Wwelcome to the Course Management Application !");
+
+            int selection;
+            do
+            {
+                Console.WriteLine("1. Create the new Group");
+                Console.WriteLine("2. Show the list of Groups");
+                Console.WriteLine("3. Edit Groups");
+                Console.WriteLine("4. Show the lilst of students in the group");
+                Console.WriteLine("5. Show the list of all students");
+                Console.WriteLine("6. Create the new Student");
+                Console.WriteLine("7. Delete any strudent");
+                Console.WriteLine("0. Exit");
+                bool result = int.TryParse(Console.ReadLine(), out selection);
+
+
+            } while (selection!=0);
         }
     }
 }
